309768e76891a87146d23a3c0d9c04042b32f156
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-03-04 11:05:38 -0800

    Add more logic to favorite patterns and add Cypress test specs

--- src/components/counters/Counters.jsx
+++ src/components/counters/Counters.jsx
@@ -2,25 +2,27 @@ import React, { useState } from 'react';
 import "./Counters.scss";
 import { MdLabelImportant, MdLabelImportantOutline, MdFavoriteBorder, MdFavorite} from "react-icons/md";
 
-export default function Counters() {
-    const [heartClick, setHeartClick] = useState({isClicked: false});
-    const [flagClick, setFlagClick] = useState({isClicked: false});
+export default function Counters({addToLikes, currentPatternId}) {
+    const [heartClick, setHeartClick] = useState(false);
+    const [flagClick, setFlagClick] = useState(false);
 
     function toggleFlag(){
         setFlagClick(prevState => !prevState)
-        console.log("flagClick: ", flagClick)
     }
 
     function toggleHeart(){
         setHeartClick(prevState => !prevState)
-        console.log("heartClick: ", heartClick)
-
+        if(heartClick){
+            addToLikes(currentPatternId);
+            console.log(currentPatternId);
+            
+        }
     }
 
     const flagIcon = flagClick ? <MdLabelImportant /> : <MdLabelImportantOutline />
     const flagIconText = flagClick ? `Helpful.` : `This is immaterial.`;
     const heartIcon = heartClick ? <MdFavorite /> : <MdFavoriteBorder />;
-    const heartIconText = flagClick ? `I want to make this.` : `Probably won't try it.`
+    const heartIconText = heartClick ? `I want to make this.` : `Probably won't try it.`
 
     return (
         <div className="counters-dash">
--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -8,20 +8,44 @@ import PatternNotes from "../patternNotes/PatternNotes";
 export default function Details(){
     const [userLiked, setUserLiked] = useState([]);
     const [userFlagged, setUserFlagged] = useState([]);
+//everything has to be turned into a string, or do that in the post?
+    const [currentPatternId, setCurrentPatternId] = useState();
 
     const location = useLocation();
     const { from } = location.state;
 
-    function trackLikes(patternNum){
+    const getPatternId = () => {
+        setCurrentPatternId(prevState => prevState = from.id);
+    }
+
+    console.log("set from.id: ", from.id, "currentPattern: ", currentPatternId)
+
+    function addToLikes(patternNum){
+        getPatternId();
         setUserLiked(prevState => {
-            return [...prevState, patternNum]
+            return [...prevState, patternNum];
         })
     }
-    
+
+    console.log("userLiked: ", userLiked)
+    console.log("from.id: ", from.id)
+    console.log("currentPatternId: ", currentPatternId)
+    //^^this function will add a like, but how do we remove a like based on the click?
+    function removeLike(patternNum){
+        setUserLiked(prevState => {
+            if(prevState.includes(patternNum)){
+                const newState = prevState.filter(e => !e === patternNum);
+                return newState;
+            }
+        })
+    }
+
+    console.log("userliked: ", userLiked)
     return(
         <div className="pattern-box">
             <Pattern key={from.id} pattern={from}/>
-            <Counters />
+            <Counters addToLikes={addToLikes} currentPatternId={currentPatternId} />
+            {/* how do I get the form.id param passed with trackLikes? */}
             <PatternNotes />
         </div>
     );
