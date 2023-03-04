0cd3349d047d4937df76e5c8940c903fd2e048ec
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-03-04 14:59:50 -0800

    Add router for error page to take user home with button click

--- src/components/error/Error.jsx
+++ src/components/error/Error.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Error.scss";
 
 export default function Error() {
     return(
         <div className="error-page">
             <h2 className="error-h2">Sorry! What you're looking for isn't here!</h2>
-            <button className="return-home">Let's go look at patterns!</button>
+            <Link to="/">
+                <button className="return-home">Let's go look at patterns!</button>
+            </Link>
         </div>
     );
 }
