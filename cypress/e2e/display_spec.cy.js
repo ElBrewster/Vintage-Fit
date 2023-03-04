b69d3a34944a8acdab9f863fd9f877e812af7449
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-03-04 13:12:45 -0800

    Add Cypress testing for the main browsing page view and for routes to other displays

--- cypress/e2e/display_spec.cy.js
+++ cypress/e2e/display_spec.cy.js
@@ -1,5 +1,28 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe("App and its main browser page", () => {
+  beforeEach(() => {
+    cy.intercept("http://localhost:5500/patterns", { method: "GET", fixture: "../fixtures/fixture.json" })
+    cy.visit("http://localhost:5173/")
+  })
+  it('Should show the main page of the app', () => {
+    cy.get("header").should("have.class", "my-header")
+    cy.get("h1").contains("Vintage Fit")
+    cy.get("nav").contains("Browse")
+  })
+
+  it("Should have a nav bar that navigates to different page views using react router", () => {
+    cy.get("ul.browse-nav").contains("Browse").click()
+      .url().should("eq", "http://localhost:5173/")
+    cy.get("ul.profile-nav").contains("My Page").click()
+      .url().should("eq", "http://localhost:5173/profile")
+    cy.get("ul.your-fit-notes-nav").contains("Fit Notes").click()
+      .url().should("eq", "http://localhost:5173/your-fit-notes")
+  })
+
+  it("Should show patterns in the main display", () => {
+    cy.get(".portraits").should("exist")
+    cy.get(".pattern-pic").should("be.visible")
+    cy.get(".portrait-heading").contains("Butterick Retro 52");
+    cy.get(".portrait-button").contains("Community Notes").click()
+      .url().should("eq", "http://localhost:5173/details/1")
   })
 })
